9e140137ee6b90dfc405829c988e5527488372ff
MohamedAIsmail <77363034+MohamedAIsmail@users.noreply.github.com>
2023-04-27 13:38:37 +0200

    Time: 0 ms (100.00%), Space: 6 MB (64.77%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -3,28 +3,19 @@ class Solution
 public:
     int reverse(int x)
     {
-        string result;
-        if (x > 0)
-            result = "";
-        else if (x < 0)
-            result = "-";
-        else
-            return 0;
+        long result = 0;
 
         while (x)
         {
-            result = result + to_string(abs(x) % 10);
+            result = result*10 + x%10;
             x /= 10;
         }
 
-        try
-        {
-            unsigned long sol = stoi(result);
-            return sol;
+        if(result > INT_MAX || result < INT_MIN){
+            return 0;
         }
-        catch (const std::out_of_range &e)
-        {
-            return 0; // or return an error message
-        }
+            
+
+        return int(result);
     }
 };
