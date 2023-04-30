e6bcc599e2d1007aa08a37defc421eb0f1258ff4
MohamedAIsmail <77363034+MohamedAIsmail@users.noreply.github.com>
2023-04-30 16:32:52 +0200

    Time: 123 ms (5.35%), Space: 58.9 MB (99.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,7 +5,7 @@ public:
     {
         int i = 0, j = height.size() - 1;
         int maxA = 0;
-        while (i < height.size() && j > 0)
+        while (i != j)
         {
             int w = j - i;
             int h = min(height[i], height[j]);
