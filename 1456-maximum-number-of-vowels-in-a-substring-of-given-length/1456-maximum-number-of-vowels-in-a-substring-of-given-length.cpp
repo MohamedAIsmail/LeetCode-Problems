d4c27123885fb2a40f85f55aae8477d293fba980
MohamedAIsmail <77363034+MohamedAIsmail@users.noreply.github.com>
2023-05-05 03:48:47 +0300

    Time: 26 ms (40.98%), Space: 9.9 MB (95.70%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     int maxVowels(string s, int k)
     {
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        unordered_map<int, int> map;
         int maxVow = 0;
         int VowCtr = 0;
 
-        for (int i = 0; i < s.size(); i++)
+        for (int i = 0; i < k; i++)
         {
-            if (i >= k && vowels.count(s[i - k]))
-            {
-                VowCtr--;
-            }
-            if (vowels.count(s[i]))
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
             {
                 VowCtr++;
             }
+        }
+        maxVow = VowCtr;
+        for (int i = k; i < s.size(); i++)
+        {
+            int j = i - k;
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                VowCtr++;
+            if (s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' || s[i - k] == 'o' || s[i - k] == 'u')
+                VowCtr--;
+
             maxVow = max(maxVow, VowCtr);
         }
-
         return maxVow;
     }
 };
