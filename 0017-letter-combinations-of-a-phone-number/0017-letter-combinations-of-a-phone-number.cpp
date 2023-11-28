3f02a668d73bdd6d32b493df8de2755fdea64c82
MohamedAIsmail <77363034+MohamedAIsmail@users.noreply.github.com>
2023-11-29 01:35:50 +0200

    Time: 3 ms (40.98%), Space: 6.9 MB (49.28%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        vector<string> res;
+        if (digits.empty())
+            return res;
+            
+        vector<string> dict = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        res.push_back("");
+        for (int i = 0; i < digits.size(); i++)
+        {
+            vector<string> temp;
+            string str = dict[digits[i] - '2'];
+            for (int j = 0; j < str.size(); j++)
+            {
+                for (int k = 0; k < res.size(); k++)
+                {
+                    temp.push_back(res[k] + str[j]);
+                }
+            }
+            res = temp;
+        }
+        return res;
+    }
+};
